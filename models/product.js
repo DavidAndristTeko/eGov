aecc3d13a7678e70f6ce79a973bd8c84de2d2dd1
DavidAndristTeko <david.andrist@edu.teko.ch>
2026-06-30 11:14:56 +0200

    Bugfixes

--- models/product.js
+++ models/product.js
@@ -1,11 +1,11 @@
 import mongoose, { Schema } from "mongoose";
 
-const productSchema = new Schema({
-  title: { type: String, default: [true, "Titel fehlt"] },
+const productschema = new Schema({
+  title: { type: String, required: [true, "Titel fehlt"] },
   description: String,
   active: { type: Boolean, default: true },
   price: { max: 9999, type: Number },
 });
 
-const product = mongoose.model("product", productSchema);
-export default Product;
+const product = mongoose.model("Product", productschema);
+export default product;
