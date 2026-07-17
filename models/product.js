c76e32105fa6dfd3f96d5a89c665dc1869488c46
MaelleEschTeko <maelle.esch@edu.teko.ch>
2026-07-17 14:22:05 +0200

    fertig stellung product.js

--- models/product.js
+++ models/product.js
@@ -1,10 +1,11 @@
 import mongoose, { Schema } from "mongoose";
 
 const productschema = new Schema({
+  productNumber: { type: Number, required: true },
   title: { type: String, required: [true, "Titel fehlt"] },
   description: String,
-  active: { type: Boolean, default: true },
-  price: { max: 9999, type: Number },
+  active: { type: Boolean, required: true, default: true },
+  price: { min: 0, max: 9999, type: Number, default: 0 },
 });
 
 const product = mongoose.model("Product", productschema);
