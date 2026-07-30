034df6d4324f61f5603f9a75805f632781c91db3
DavidAndristTeko <david.andrist@edu.teko.ch>
2026-07-30 18:12:57 +0200

    Passwort Hashing gemacht & getestet

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose"; //Schema Klasse wird importiert, damit man später im Code einsch "Schema" schreiben kann anstatt "mongoose.Schema"
+import bcrypt from "bcrypt"; //Bcrypt wird importiert für Passwort Hashing
 
 //Schema wird definiert
 const userSchema = new Schema({
@@ -34,6 +35,16 @@ const userSchema = new Schema({
     minLength: 8,
   },
   userActive: { type: Boolean, required: true, default: true },
+});
+
+//Bevor eine .save() Operation auf das Model ausgeführt wird..
+userSchema.pre("save", async function (next) {
+  //...und wenn das Passwort auch wirklich angepasst wurde...
+  if (!this.isModified("password")) {
+    //Reguläre anstatt Pfeilfunktion wird genutzt, da this in Pfeilfunktion nicht verlässlich auf das Objekt verweisen würde
+    return; //Falls Passwort nicht angepasst wurde aus Funktion treten
+  }
+  this.password = await bcrypt.hash(this.password, 10); //...das Passwort hashen. 10 Salt rounds, da dies der konventionel genutzte wert ist.
 });
 
 const user = mongoose.model("User", userSchema); //Schema wird als Model registriert
