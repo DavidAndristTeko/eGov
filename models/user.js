839f7891756275774377aed6f4ecf20862e122cf
DavidAndristTeko <david.andrist@edu.teko.ch>
2026-07-31 14:44:39 +0200

    Pw-hashing für Pw-Änderung umgesetzt

--- models/user.js
+++ models/user.js
@@ -40,11 +40,25 @@ const userSchema = new Schema({
 //Bevor eine .save() Operation auf das Model ausgeführt wird..
 userSchema.pre("save", async function (next) {
   //...und wenn das Passwort auch wirklich angepasst wurde...
-  //Reguläre anstatt Pfeilfunktion wird genutzt, da this in Pfeilfunktion nicht verlässlich auf das Objekt verweisen würde
+  //Reguläre anstatt Pfeilfunktion wird genutzt, da "this" in Pfeilfunktion nicht verlässlich auf das Objekt verweisen würde
   if (!this.isModified("password")) {
     return; //Falls Passwort nicht angepasst wurde aus Funktion treten
   }
   this.password = await bcrypt.hash(this.password, 10); //...das Passwort hashen. 10 Salt rounds, da dies der konventionel genutzte wert ist.
+});
+
+/*Die obere hook greift, wenn ein neuer User erstellt wird. Sollte ein User jedoch sein Passwort ändern, läuft dies nicht über POST, sondern PUT,
+welches dann "findByIdAndUpdate" nutzt. Das heisst wir brauchen eine 2. hook, die das Passwort hasht, wenn ein User dieses bei seinem bereits existierenden
+Konto aktualisiert. Wir erledigen dies über eine findOneAndUpdate hook. PUT läuft über findByIdAndUpdate, was einfach eine findOneAndUpdate Abfrage
+abfeuert, welche auf die ID konfiguriert ist. Somit können wir diese Abfragen mit dieser Hook abfangen.*/
+userSchema.pre("findOneAndUpdate", async function () {
+  /*In einer findOneAndUpdate Hook bezieht sich "this" nicht auf das Nutzerobjekt selber, sondern die Query. Darum müssen wir zuerst über "getUpdate" den
+  tatsächlichen Update Payload abholen.*/
+  const update = this.getUpdate();
+  //Es wird geprüft ob User überhaupt das pw angepasst hat um unnötiges re-hashen zu vermeiden
+  if (update.password) {
+    update.password = await bcrypt.hash(update.password, 10);
+  }
 });
 
 const user = mongoose.model("User", userSchema); //Schema wird als Model registriert
