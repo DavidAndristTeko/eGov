8ffacc3bed25a10d4b97eaca36c4611f189c79d8
DavidAndristTeko <david.andrist@edu.teko.ch>
2026-09-15 13:50:49 +0200

    If-Abfrage aus Hook entfernt, da dieser hook nur für erstellen von Usern genutzt wird und nicht prüfen muss, ob das Passwort eingegeben wurde.

--- backend/models/user.js
+++ backend/models/user.js
@@ -41,13 +41,8 @@ const userSchema = new Schema({
 });
 
 //Bevor eine .save() Operation auf das Model ausgeführt wird..
+//(Reguläre anstatt Pfeilfunktion wird genutzt, da "this" in Pfeilfunktion nicht verlässlich auf das Objekt verweisen würde)
 userSchema.pre("save", async function (next) {
-  /*...und wenn das Passwort auch wirklich eingegeben wurde...
-  (Reguläre anstatt Pfeilfunktion wird genutzt, da "this" in Pfeilfunktion nicht verlässlich auf das Objekt verweisen würde)*/
-  if (!this.isModified("password")) {
-    //Falls Passwort nicht angepasst wurde aus Funktion treten
-    return;
-  }
   //...das Passwort hashen. 10 Salt rounds, da dies der konventionel genutzte wert ist.
   this.password = await bcrypt.hash(this.password, 10);
 });
